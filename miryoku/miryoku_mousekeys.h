dd721af1ad5a056ec3f4b67bac00ee4347cb9de0
Giulio Bilboc <fortnitebr.italia1@gmail.com>
2025-04-09 15:20:12 +0200

    Update miryoku_mousekeys.h

--- miryoku/miryoku_mousekeys.h
+++ miryoku/miryoku_mousekeys.h
@@ -11,7 +11,7 @@
 #define U_MS_L &mmv MOVE_LEFT
 #define U_MS_R &mmv MOVE_RIGHT
 #define U_MS_U &mmv MOVE_UP
-#define U_WH_D U_NU
-#define U_WH_L U_NU
-#define U_WH_R U_NU
-#define U_WH_U U_NU
+#define U_WH_D &msc SCRL_DOWN
+#define U_WH_L &msc SCRL_LEFT
+#define U_WH_R &msc SCRL_RIGHT
+#define U_WH_U &msc SCRL_UP
